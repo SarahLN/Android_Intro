0e5e353b179c9f8d589a32b8c78c1552ae9e569d
SarahLN <noles.sarah@gmail.com>
2018-02-13 20:04:53 -0600

    Finishing 8.3

--- app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
+++ app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class CreateUserActivity extends AppCompatActivity {
@@ -17,6 +18,24 @@ public class CreateUserActivity extends AppCompatActivity {
     public void onClick(View view) {
         EditText input = (EditText) findViewById(R.id.username);
         String string = input.getText().toString();
+
+        Boolean male = true;
+
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.gender);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.male:
+                        male = true;
+                        break;
+                    case R.id.female:
+                        male = false;
+                        break;
+                }
+            }
+        });
+
         Toast.makeText(this, "User " + string + " created.", Toast.LENGTH_LONG).show();
     }
 }
