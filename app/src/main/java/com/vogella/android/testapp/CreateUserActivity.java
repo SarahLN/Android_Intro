03c8a56967ab80dca20c145ac9647ed1586f0079
SarahLN <noles.sarah@gmail.com>
2018-02-13 19:30:40 -0600

    After section 7.3

--- app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
+++ app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
@@ -3,11 +3,11 @@ package com.vogella.android.testapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_user);
     }
 }
